21b3f4f22e3cd4c77b0460a22d470afbddd08a16
yagger <yagger.yeah.net>
2017-02-08 23:47:46 +0800

    all

--- __inti__.py
+++ __inti__.py
@@ -7,10 +7,10 @@ DEBUG = True
 SECRET_KEY = 'development key'
 bootstrap = Bootstrap(app)
 
-@app.route('/index/<name>')
-def index(name):
-    return render_template('index.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(port=5003)
 

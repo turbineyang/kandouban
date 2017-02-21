8c576d01335ff8525cf24056f66fb5519271142b
yagger <yagger.yeah.net>
2017-02-21 15:09:52 +0800

    增加选电影功能

--- __init__.py
+++ __init__.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, url_for, session, redirect, flash
 from flask_bootstrap import Bootstrap
-from form import NameForm
+from form import FlaskForm
+from pymongo import MongoClient
 
 app = Flask('kandouban')
 DEBUG = True
 app.config['SECRET_KEY'] = 'development key'
 bootstrap = Bootstrap(app)
+client = MongoClient('127.0.0.1', 27017)
+db = client.hello
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = FlaskForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('look like your name has changed!!')
+        user = session.get('name')
+        if user is not None and user != form.name.data:
+            flash('sign up successful!!')
         session['name'] = form.name.data
+        session['password'] = form.password.data
+        db.collec.insert({'name': session['name'],
+                          'password': hash(session['password'])})
         return redirect(url_for('index'))
-    return render_template('in.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+@app.route('/movie')
+def movie():
+    return render_template('movie_select.html')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5001)
--- form.py
+++ form.py
@@ -1,8 +1,10 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import Required, Length, Email
 
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+class FlaskForm(Form):
+    name = StringField('What is your name?', validators=[Required(), Length(1,10)])
+    password = PasswordField('input your password', validators=[Required()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('注册')

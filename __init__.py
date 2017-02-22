9f6ca1192ad1523a2e4ca87cf94c52799cbcec18
yagger <yagger.yeah.net>
2017-02-22 19:03:43 +0800

    增加翻页

--- __init__.py
+++ __init__.py
@@ -8,11 +8,11 @@ DEBUG = True
 app.config['SECRET_KEY'] = 'development key'
 bootstrap = Bootstrap(app)
 client = MongoClient('127.0.0.1', 27017)
-db = client.hello
+db = client.douban
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     form = FlaskForm()
     if form.validate_on_submit():
         user = session.get('name')
@@ -22,13 +22,18 @@ def index():
         session['password'] = form.password.data
         db.collec.insert({'name': session['name'],
                           'password': hash(session['password'])})
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+        return redirect(url_for('login'))
+    return render_template('login.html', form=form, name=session.get('name'))
 
 
-@app.route('/movie')
+@app.route('/')
 def movie():
-    return render_template('movie_select.html')
+    text = '-'
+    results = db.movie.find({"评分": {"$gt": '9.5'}})
+    text = '-'
+    item = map(lambda a: a['电影名'], results)
+    movie_abstract = text.join(item)
+    return render_template('movie.html', movie_abstract=movie_abstract)
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(port=5006)
--- mongo_connect.py
+++ mongo_connect.py
@@ -4,24 +4,19 @@ import pymongo
 client = pymongo.MongoClient('127.0.0.1', 27017)
 
 # 创建数据库
-db = client.test
+db = client.douban
 
 # 插入
-for i in range(10):
-    db.my_collection.insert_one({'b': i})
-for i in range(10):
-    db.movie.insert_one({'b': i})
+# for i in range(10):
+#     db.my_collection.insert_one({'b': i})
+# for i in range(10):
+#     db.movie.insert_one({'b': i})
 
 # 更新多条记录要使用$inc
-db.movie.update_many({'a': 2}, {'$inc': {'x': 3, 'g': 9}})
-
+results = db.movie.find({'评分': {'$gt': '9.4'}})
+text = '-'
+item = map(lambda a:a['电影名'],results)
+print(text.join(item))
 # 查找a小于5，或者b大于等于2的记录
-for i in db.movie.find({'$or': [{'a': {'$lt': 5}}, {'b': {'$gte': 2}}]}):
-    print(i)
 
-mydb = client.kandouban
 
-data = {'name': 'yagger',
-        'age': '25',
-        'sex': 'male'}
-mydb.movie.insert(data)
